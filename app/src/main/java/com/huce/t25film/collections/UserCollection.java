a7a60b6caf9b68557b9c530eaa2f7d884fb69328
TamHM <hoangminhtam1602@gmail.com>
2023-11-16 14:48:06 +0700

    m

--- app/src/main/java/com/huce/t25film/collections/UserCollection.java
+++
@@ -1,9 +0,0 @@
-package com.huce.t25film.collections;
-
-import com.huce.t25film.model.User;
-
-import java.util.List;
-
-public class UserCollection {
-    public List<User> users;
-}
--- app/src/main/java/com/huce/t25film/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.huce.t25film.dao;
-
-import androidx.room.Dao;
-
-@Dao
-public class UserDao {
-}
